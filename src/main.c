f203f8b2a711dcdc2cad7b9536f874a15715d51b
AskBre <askvb@online.no>
2016-02-04 00:11:14 +0100

    Added basic counting of notes

--- src/main.c
+++ src/main.c
@@ -4,17 +4,16 @@
 #include <libxml/tree.h>
 #include <libxml/xpath.h>
 
-#include "markovAnalyser.h"
-
 struct Note {
-	char name;
+	char *name;
 	int count;
 	float chance;
 };
 
 struct TransitionMatrix {
 	struct Note *notes;
-	int size;
+	int elements;
+	int allocated;
 };
 
 // XML-reading
@@ -39,15 +38,56 @@ xmlDocPtr getDoc(char *docName) {
 	return doc;
 }
 
-int addNoteToMatrix(char n, struct TransitionMatrix tm) {
-	// Check if n exists in tm.name
-	// If then tm.count++
-	// if not then new tm tm.name=n
+int getNotePos(char *n, struct TransitionMatrix *tm) {
+	int notePos = -1;
+	for(int i=0; i<tm->allocated; i++){
+		if(strcmp(n, tm->notes[i].name) == 0) notePos = i;
+	}
+	return notePos;
+}
+
+int addNoteToMatrix(char *n, struct TransitionMatrix *tm) {
+
+	printf("Got note: %s\n", n);
+	int notePos = getNotePos(n, tm);
+	printf("Notepos is %i\n", notePos);
+
+	if(notePos >= 0) {
+		printf("Already here!\n");
+		tm->notes[notePos].count++;
+	} else {
+		printf("Adding it now\n");
+		if(tm->elements < tm->allocated) {
+			struct Note *note = &tm->notes[tm->elements];
+			note->name = n;
+			note->count = 1;
+			tm->elements++;
+		} else {
+			printf("Allocated %i and elements %i\n", tm->allocated, tm->elements);
+			fprintf(stderr, "No more room in transition matrix. Sure you've got the notes right?\n");
+			return 1;
+		}
+	}
+
 	return 0;
 }
 
 struct TransitionMatrix getTransitionMatrix(xmlDocPtr doc) {
+	// Setup the transition matrix
 	struct TransitionMatrix tm;
+	tm.allocated = 12;
+	tm.elements = 0;
+	void * _notes = malloc(tm.allocated * sizeof(struct TransitionMatrix));
+
+	if(!_notes) {
+		fprintf(stderr,  "Couldn't allocate notes for the transition matrix");
+	}
+
+	tm.notes = (struct Note *) _notes;
+
+	for(int i=0; i<tm.allocated; i++){
+		tm.notes[i].name = (char *) calloc(2, sizeof(char));
+	}
 
 	////////////////////
 	// Get all the notes!
@@ -62,22 +102,28 @@ struct TransitionMatrix getTransitionMatrix(xmlDocPtr doc) {
 	xmlNotes = notePath->nodesetval;
 	int noteCount = xmlNotes->nodeNr;
 
-	char **noteNames = calloc(noteCount, sizeof(struct Note));
-
 	for(int i=0; i< xmlNotes->nodeNr; i++) {
-		char xmlNoteName;
-		xmlNoteName = (char)xmlNodeListGetString(doc, xmlNotes->nodeTab[i]->xmlChildrenNode, 1)[0];
-		printf("Note: %c\n", xmlNoteName);
+		char *xmlNoteName;
+		xmlNoteName = (char *)xmlNodeListGetString(doc, xmlNotes->nodeTab[i]->xmlChildrenNode, 1);
 
-		noteNames[i] = malloc(sizeof(xmlNoteName));
-//		strcpy(noteNames[i], xmlNoteName);
-
-		addNoteToMatrix(xmlNoteName);
+		addNoteToMatrix(xmlNoteName, &tm);
 	}
 	
 	xmlXPathFreeObject(notePath);
 
 	return tm;
+}
+
+int printTransitionMatrix(struct TransitionMatrix *tm) {
+
+	printf("\n\n\nLet's print the transition matrix!\n");
+	for(int i=0; i<tm->elements; i++) {
+		struct Note note = tm->notes[i];
+		printf("Element %i:\n", i);
+		printf("\tName:\t%s\n\tCount:\t%i\n\tChance:\t%f\n\n", note.name, note.count, note.chance);
+	}
+
+	return 0;
 }
 
 int main(int argc, char **argv) {
@@ -91,6 +137,8 @@ int main(int argc, char **argv) {
 	doc = getDoc(argv[1]);
 	
 	struct TransitionMatrix tm = getTransitionMatrix(doc);
+
+		printTransitionMatrix(&tm);
 
 	xmlFreeDoc(doc);
 	xmlCleanupParser();
