bd1aa4efcb15f86447d336fd53fad6d69364c97b
AskBre <askvb@online.no>
2016-04-10 11:26:00 +0200

    Comparing of nodes, not only names

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <libxml/tree.h>
 #include <libxml/xpath.h>
 
+// General xml parsing stuff
+//////////////////////////////
 xmlDoc *getDoc(char *docName) {
 	xmlDocPtr doc = NULL;
 
@@ -20,12 +22,28 @@ xmlDoc *getDoc(char *docName) {
 	xmlNode *root_element = xmlDocGetRootElement(doc);
 
 	if(xmlStrcmp(root_element->name, (const xmlChar *) "score-partwise")) {
-		fprintf(stderr, "This doesn't seem to be a correct musixXml document");
+		fprintf(stderr, "This doesn't seem to be a correct musixXml document \n");
 		xmlFreeDoc(doc);
 		return NULL;
 	}
 
 	return doc;
+}
+
+xmlNodeSet *makeNodeSet(xmlDoc *doc, char *_keyword) {
+	xmlNodeSet *set;
+	xmlXPathObject *notePath;
+	xmlChar *keyword = (xmlChar *) _keyword;
+
+	// Set notePath
+	notePath = xmlXPathEvalExpression(keyword, xmlXPathNewContext(doc));
+
+	// Get notenames
+	set = notePath->nodesetval;
+
+	if(!set) fprintf(stderr, "Couldn't make set from %s \n", (char *) keyword);
+
+	return set;
 }
 
 xmlNode *getChildNode(xmlNode *parent, char *childName) {
@@ -38,8 +56,32 @@ xmlNode *getChildNode(xmlNode *parent, char *childName) {
 		}
 	}
 
+	if(!child) fprintf(stderr, "Couldn't find child %s \n", childName);
+
 	return child;
 }
+
+int removeChildNode(xmlNode *parent, char *childName) {
+	xmlNode *cur = NULL;
+	xmlNode *child = NULL;
+
+	for(cur = parent->children; cur; cur = cur->next) {
+		if(xmlStrcmp(cur->name, (const xmlChar *) childName) == 0) {
+			child = cur;
+		}
+	}
+
+	if(!child) {
+		fprintf(stderr, "Couldn't find child %s \n", childName);
+		return 1;
+	} else {
+		xmlUnlinkNode(child);
+		xmlFreeNode(child);
+		return 0;
+	}
+}
+
+//////////////////////////////
 
 // Getting of note properties
 //////////////////////////////
@@ -47,37 +89,42 @@ char *getNoteName(xmlNode *note) {
 	xmlNode *pitch = getChildNode(note, "pitch");
 	xmlNode *step = getChildNode(pitch, "step");
 
-	return (char *) xmlNodeGetContent(step->xmlChildrenNode);
+	if(step) return (char *) xmlNodeGetContent(step->xmlChildrenNode);
+	else return NULL;
 }
 
 char *getNoteOctave(xmlNode *note) {
 	xmlNode *pitch = getChildNode(note, "pitch");
 	xmlNode *octave = getChildNode(pitch, "octave");
 
-	return (char *) xmlNodeGetContent(octave->xmlChildrenNode);
+	if(octave) return (char *) xmlNodeGetContent(octave->xmlChildrenNode);
+	else return NULL;
 }
 
+char *getNoteDuration(xmlNode *note) {
+	xmlNode *dur = getChildNode(note, "duration");
+
+	if(dur) return (char *) xmlNodeGetContent(dur->xmlChildrenNode);
+	else return NULL;
+}
+
+
+int stripNotes(xmlNodeSet *notes) {
+	// Retain only the core components of note for comparison
+	for(int i=0; i<notes->nodeNr; i++) {
+		removeChildNode(notes->nodeTab[i], "stem");
+		removeChildNode(notes->nodeTab[i], "type");
+	}
+
+	return 0;
+}
 //////////////////////////////
 
-xmlNodeSet *makeNodeSet(xmlDoc *doc, char *_keyword) {
-
-	xmlNodeSet *set;
-	xmlXPathObject *notePath;
-	xmlChar *keyword = (xmlChar *) _keyword;
-
-	// Set notePath
-	notePath = xmlXPathEvalExpression(keyword, xmlXPathNewContext(doc));
-
-	// Get notenames
-	set = notePath->nodesetval;
-
-	return set;
-}
 
 int main(int argc, char **argv) {
 	// Load xml-file
 	if (argc != 2) {
-		fprintf(stderr, "Need a document to operate on");
+		fprintf(stderr, "Need a document to operate on \n");
 		return 1;
 	}
 
@@ -89,12 +136,20 @@ int main(int argc, char **argv) {
 	root = xmlDocGetRootElement(doc);
 	notes = makeNodeSet(doc, "//note");
 
+	if(stripNotes(notes)) {
+		fprintf(stderr, "Couldn't strip the notes \n");
+		return 1;
+	}
+
 	for(int i=0; i<notes->nodeNr; i++) {
 		int count = 0;
 		for(int j=0; j<notes->nodeNr; j++) {
-			if(strcmp(getNoteName(notes->nodeTab[i]), getNoteName(notes->nodeTab[j])) == 0) count++;
+			char *note1 = (char *) xmlNodeGetContent(notes->nodeTab[i]);
+			char *note2 = (char *) xmlNodeGetContent(notes->nodeTab[j]);
+			if(strcmp(note1, note2) == 0) count++;
 		}
-		printf("Note %s has %i instances \n", getNoteName(notes->nodeTab[i]), count);
+
+		printf("Note %s %s has %i instances \n", getNoteName(notes->nodeTab[i]), getNoteDuration(notes->nodeTab[i]), count);
 	}
 
 	xmlFreeDoc(doc);
